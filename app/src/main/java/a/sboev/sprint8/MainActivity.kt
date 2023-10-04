6f6408ecab82046b1134e14a4a78d851ef304fe7
Andrey <a.sboev@teleswyz.ru>
2023-10-04 20:02:01 +0300

    tool bar

--- app/src/main/java/a/sboev/sprint8/MainActivity.kt
+++ app/src/main/java/a/sboev/sprint8/MainActivity.kt
@@ -2,10 +2,17 @@ package a.sboev.sprint8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setTitle(R.string.app_name)
+        toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }
